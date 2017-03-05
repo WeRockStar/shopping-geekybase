da5e3f222f030989174ad2b275a89a05a2e896bb
WeRockStar <kotchaphan.m@gmail.com>
2017-03-05 14:02:31 +0700

    Add empty fragment

---
+++ app/src/main/java/com/sprint3r/shoppingmicroserviceclass/ProductListFragment.java
@@ -0,0 +1,29 @@
+package com.sprint3r.shoppingmicroserviceclass;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProductListFragment extends Fragment {
+
+
+    public ProductListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_product_list, container, false);
+    }
+
+}
