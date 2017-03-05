7d98eb50086da408b2016b9a06e200ae82760ba1
piya_l <piya.l@g-able.com>
2017-03-05 16:06:52 +0700

    Add okhttp3 http logging

--- app/src/main/java/com/sprint3r/shoppingmicroserviceclass/service/RetrofitService.java
+++ app/src/main/java/com/sprint3r/shoppingmicroserviceclass/service/RetrofitService.java
@@ -7,6 +7,8 @@ import com.sprint3r.shoppingmicroserviceclass.domain.Tracking;
 import java.util.List;
 
 import io.reactivex.Observable;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -18,10 +20,14 @@ public class RetrofitService {
 
     public static Retrofit getRetrofitService() {
         if (retrofit == null) {
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient client = new OkHttpClient.Builder().addInterceptor(loggingInterceptor).build();
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .client(client)
                     .build();
         }
         return retrofit;
