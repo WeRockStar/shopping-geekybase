d3f3feb9d2d77690be8453cfd240fd4aa5d0c833
WeRockStar <kotchaphan.m@gmail.com>
2017-03-05 14:09:58 +0700

    Prepare list product

--- app/src/main/java/com/sprint3r/shoppingmicroserviceclass/MainActivity.java
+++ app/src/main/java/com/sprint3r/shoppingmicroserviceclass/MainActivity.java
@@ -9,5 +9,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goToProductList();
+    }
+
+    private void goToProductList() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.containProduct, new ProductListFragment())
+                .commit();
     }
 }
--- app/src/main/java/com/sprint3r/shoppingmicroserviceclass/ProductListFragment.java
+++ app/src/main/java/com/sprint3r/shoppingmicroserviceclass/ProductListFragment.java
@@ -3,27 +3,41 @@ package com.sprint3r.shoppingmicroserviceclass;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ProductListFragment extends Fragment {
 
 
+    RecyclerView recyclerView;
+    SwipeRefreshLayout swipeRefreshLayout;
+
     public ProductListFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_product_list, container, false);
+
+        initialView(view);
+        setUpRecyclerView();
+        return view;
     }
 
+    private void initialView(View view) {
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefresh);
+    }
+
+    private void setUpRecyclerView() {
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+    }
 }
