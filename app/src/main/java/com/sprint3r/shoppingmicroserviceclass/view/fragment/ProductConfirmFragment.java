eea0942443d0795f3fb88ce8d4c5995f48c77cdb
WeRockStar <kotchaphan.m@gmail.com>
2017-03-05 15:36:42 +0700

    Prepare view confirm details

--- app/src/main/java/com/sprint3r/shoppingmicroserviceclass/view/fragment/ProductConfirmFragment.java
+++ app/src/main/java/com/sprint3r/shoppingmicroserviceclass/view/fragment/ProductConfirmFragment.java
@@ -1,7 +1,6 @@
 package com.sprint3r.shoppingmicroserviceclass.view.fragment;
 
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
@@ -9,15 +8,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.sprint3r.shoppingmicroserviceclass.R;
 import com.sprint3r.shoppingmicroserviceclass.domain.Product;
+import com.sprint3r.shoppingmicroserviceclass.presenter.OrderPresenter;
 
-public class ProductConfirmFragment extends Fragment {
+public class ProductConfirmFragment extends Fragment implements OrderPresenter.OrderView {
 
     private static final String EXTRA_PRODUCT = "EXTRA_PRODUCT";
 
+    OrderPresenter presenter;
+
+    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
     Button btnConfirm;
+    EditText edtFirstName;
+    EditText edtLastName;
+    EditText edtDescription;
 
     public static ProductConfirmFragment newInstance(Product products) {
         Bundle args = new Bundle();
@@ -35,14 +42,19 @@ public class ProductConfirmFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_product_confirm, container, false);
+
+        presenter = new OrderPresenter();
+        presenter.setView(this);
+
         btnConfirm = (Button) view.findViewById(R.id.btnConfirm);
+        edtFirstName = (EditText) view.findViewById(R.id.edtFirstName);
+        edtLastName = (EditText) view.findViewById(R.id.edtLastName);
+        edtDescription = (EditText) view.findViewById(R.id.edtDescription);
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setMessage("คุณยืนยันการทำรายการหรือไม่?");
-
         btnConfirm.setOnClickListener(v -> {
             builder.setPositiveButton("ตกลง", (dialog, which) -> {
-
+                // presenter.order(getProduct());
             });
             builder.setNegativeButton("ยกเลิก", (dialog, which) -> {
             });
@@ -53,4 +65,18 @@ public class ProductConfirmFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void render(String trackingNumber) {
+        builder.setMessage("Tracking number : " + trackingNumber);
+        builder.show();
+    }
+
+    public Product getProduct() {
+        return getArguments().getParcelable(EXTRA_PRODUCT);
+    }
+
+    @Override
+    public void error(String msg) {
+
+    }
 }
